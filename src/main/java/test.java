871b54bf011cd161d64c7f693146ef162c4bbac6
yabo083 <103875886+yabo083@users.noreply.github.com>
2022-09-04 21:27:37 +0800

    pull commit

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -6,7 +6,7 @@ public class test {
         System.out.println("master commit");
         System.out.println("hot-fix commit");
         System.out.println("push commit");
-
+        System.out.println("pull commit");
 
 
     }
