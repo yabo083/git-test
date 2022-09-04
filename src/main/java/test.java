2f859e2c54eeb694d7c4f5c2097e7d6461d3f084
Yabo <2657455842@qq.com>
2022-09-04 21:01:26 +0800

    hot-fix commit

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,5 +2,6 @@ public class test {
     public void main(String[] args) {
         System.out.println("hello git by idea");
         System.out.println("hello git by idea1");
+        System.out.println("hello git by idea2");
     }
 }
